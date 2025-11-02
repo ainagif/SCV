dbf378e8706a2bbc565ff48ed5848beb887d0581
ainagif <ainasafura0550@gmail.com>
2025-11-02 17:51:10 +0800

    Update Identifying Correlations between Risk and Life Outcome.py

--- Identifying Correlations between Risk and Life Outcome.py
+++ Identifying Correlations between Risk and Life Outcome.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 
 
